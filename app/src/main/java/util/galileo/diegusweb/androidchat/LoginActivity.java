5f81a194dfd74a3482cd86e9e398a03287e43687
Diego Rueda <diego.rueda2@gmail.com>
2016-06-09 22:02:41 -0400

    ButterKnife init

--- app/src/main/java/util/galileo/diegusweb/androidchat/LoginActivity.java
+++ app/src/main/java/util/galileo/diegusweb/androidchat/LoginActivity.java
@@ -2,12 +2,40 @@ package util.galileo.diegusweb.androidchat;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.RelativeLayout;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class LoginActivity extends AppCompatActivity {
+
+    @BindView(R.id.editTxtEmail)
+    EditText inputEmail;
+    @BindView(R.id.editTxtPassword)
+    EditText inputPassword;
+    @BindView(R.id.progressBar)
+    ProgressBar progressBar;
+    @BindView(R.id.layoutMainContainer)
+    RelativeLayout container;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.btnSignin)
+    public void handleSignin(){
+        Log.e("AndroidChat", inputEmail.getText().toString());
+    }
+
+    @OnClick(R.id.btnSignup)
+    public void handleignup(){
+        Log.e("AndroidChat", inputEmail.getText().toString());
     }
 }
