98cfa1137b5628af355effb4ba753917b73781db
Diego Rueda <diego.rueda2@gmail.com>
2016-06-11 16:09:45 -0400

    Vista implement login

--- app/src/main/java/util/galileo/diegusweb/androidchat/LoginActivity.java
+++ app/src/main/java/util/galileo/diegusweb/androidchat/LoginActivity.java
@@ -1,8 +1,12 @@
 package util.galileo.diegusweb.androidchat;
 
+import android.content.Intent;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.RelativeLayout;
@@ -10,8 +14,11 @@ import android.widget.RelativeLayout;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import util.galileo.diegusweb.androidchat.login.LoginPresenter;
+import util.galileo.diegusweb.androidchat.login.LoginView;
+import util.galileo.diegusweb.androidchat.ontactlist.ContactListActivity;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements LoginView {
 
     @BindView(R.id.editTxtEmail)
     EditText inputEmail;
@@ -22,6 +29,13 @@ public class LoginActivity extends AppCompatActivity {
     @BindView(R.id.layoutMainContainer)
     RelativeLayout container;
 
+    @BindView(R.id.btnSignin)
+    Button btnSignIn;
+    @BindView(R.id.btnSignup)
+    Button btnSignUp;
+
+    private LoginPresenter loginPresenter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,13 +43,69 @@ public class LoginActivity extends AppCompatActivity {
         ButterKnife.bind(this);
     }
 
-    @OnClick(R.id.btnSignin)
-    public void handleSignin(){
-        Log.e("AndroidChat", inputEmail.getText().toString());
+    @Override
+    public void disableInputs() {
+        setInputs(false);
     }
 
-    @OnClick(R.id.btnSignup)
-    public void handleignup(){
-        Log.e("AndroidChat", inputEmail.getText().toString());
+    @Override
+    public void enableInputs() {
+        setInputs(true);
+    }
+
+    @Override
+    public void handleSignIn() {
+        loginPresenter.validateLogin(inputEmail.getText().toString(),
+                inputPassword.getText().toString());
+    }
+
+    @Override
+    @OnClick(R.id.btnSignin)
+    public void handleSignUp() {
+        loginPresenter.registerNewUser(inputEmail.getText().toString(),
+                inputPassword.getText().toString());
+    }
+
+    @Override
+    public void hideProgress() {
+        progressBar.setVisibility(View.GONE);
+    }
+
+    @Override
+    public void showProgress() {
+        progressBar.setVisibility(View.VISIBLE);
+    }
+
+
+    @Override
+    public void loginError(String error) {
+        inputPassword.setText("");
+        String msgError = String.format(getString(R.string.login_error_message_signin), error);
+        inputPassword.setError(msgError);
+    }
+
+    @Override
+    public void navigateToMainScreen() {
+        startActivity(new Intent(this, ContactListActivity.class));
+    }
+
+    @Override
+    public void newUserError(String error) {
+        inputPassword.setText("");
+        String msgError = String.format(getString(R.string.login_error_message_signup), error);
+        inputPassword.setError(msgError);
+    }
+
+    @Override
+    public void newUserSuccess() {
+        //snakbar
+        Snackbar.make(container, R.string.login_notice_message_signup, Snackbar.LENGTH_SHORT).show();
+    }
+
+    private void setInputs(boolean enabled){
+        btnSignIn.setEnabled(true);
+        btnSignUp.setEnabled(true);
+        inputEmail.setEnabled(true);
+        inputPassword.setEnabled(true);
     }
 }
---
+++ app/src/main/java/util/galileo/diegusweb/androidchat/ontactlist/ContactListActivity.java
@@ -0,0 +1,15 @@
+package util.galileo.diegusweb.androidchat.ontactlist;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import util.galileo.diegusweb.androidchat.R;
+
+public class ContactListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contact_list);
+    }
+}
