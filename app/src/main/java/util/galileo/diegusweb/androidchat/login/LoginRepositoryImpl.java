b6207472107332e0fb67aceefb78c59d4c52431c
Diego Rueda <diego.rueda2@gmail.com>
2016-06-13 20:10:38 -0400

    Signin y Signup

--- app/src/main/java/util/galileo/diegusweb/androidchat/login/LoginRepositoryImpl.java
+++ app/src/main/java/util/galileo/diegusweb/androidchat/login/LoginRepositoryImpl.java
@@ -2,11 +2,18 @@ package util.galileo.diegusweb.androidchat.login;
 
 import android.util.Log;
 
+import com.firebase.client.AuthData;
+import com.firebase.client.DataSnapshot;
 import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+import com.firebase.client.ValueEventListener;
+
+import java.util.Map;
 
 import util.galileo.diegusweb.androidchat.domain.FirebaseHelper;
 import util.galileo.diegusweb.androidchat.lib.EventBus;
 import util.galileo.diegusweb.androidchat.lib.GreenRobotEventBus;
+import util.galileo.diegusweb.androidchat.login.entities.User;
 import util.galileo.diegusweb.androidchat.login.events.*;
 
 /**
@@ -14,19 +21,72 @@ import util.galileo.diegusweb.androidchat.login.events.*;
  */
 public class LoginRepositoryImpl implements LoginRepository {
     private FirebaseHelper helper;
+    private Firebase dataReference;
+    private Firebase myUserReference;
 
     public LoginRepositoryImpl() {
+
         helper = FirebaseHelper.getInstance();
+        dataReference = helper.getDataReference();
+        myUserReference = helper.getMyUserReference();
     }
 
     @Override
-    public void signUp(String email, String password) {
+    public void signUp(final String email, final String password) {
+
         Log.e("LoginHelperImpl","signup");
+        dataReference.createUser(email, password, new Firebase.ValueResultHandler<Map<String, Object>>() {
+            @Override
+            public void onSuccess(Map<String, Object> stringObjectMap) {
+                postEvent(LoginEvent.onSignInSuccess);
+                signIn(email, password);
+            }
+
+            @Override
+            public void onError(FirebaseError firebaseError) {
+                postEvent(LoginEvent.onSignUpError, firebaseError.getMessage());
+            }
+        });
     }
 
     @Override
     public void signIn(String email, String password) {
+
         Log.e("LoginHelperImpl","signIn");
+        dataReference.authWithPassword(email, password, new Firebase.AuthResultHandler() {
+            @Override
+            public void onAuthenticated(AuthData authData) {
+                dataReference = helper.getDataReference();
+                myUserReference.addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        User currentUser = dataSnapshot.getValue(User.class);
+
+
+                        if(currentUser == null){
+                            String email = helper.getAuthUserEmail();
+                            if(email != null){
+                                currentUser = new User();
+                                myUserReference.setValue(currentUser);
+                            }
+                        }
+                        helper.changeUserConnectionStatus(User.ONLINE);
+                        postEvent(LoginEvent.onSignInSuccess);
+                    }
+
+                    @Override
+                    public void onCancelled(FirebaseError firebaseError) {
+
+                    }
+                });
+
+            }
+
+            @Override
+            public void onAuthenticationError(FirebaseError firebaseError) {
+                postEvent(LoginEvent.onSignUpError, firebaseError.getMessage());
+            }
+        });
     }
 
     @Override
